7212975790d52ec748f1338494ad269e1a155fdb
adldotori <adldotori@gmail.com>
2018-06-16 05:26:26 +0900

    title 넘어오면 db와 실제파일 모두 삭제

---
+++ news_delete_submit.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>나딱뉴스_기사삭제</title>
+  </head>
+  <body>
+<?php
+  $title = $_POST["title"];
+  $conn = mysqli_connect('localhost','root','taeho','database');
+  $query1 ="select contents from news where title=$title";
+  $row = mysqli_fetch_array(mysqli_query($conn,$query1));
+  $contentTitle = $row[0];
+  $root = $_SERVER['DOCUMENT_ROOT'];
+  $path = $root."/news/".$contentTitle;
+  unlink($path);
+  $query2 ="delete from news where title=$title";
+  mysqli_query($conn,$query2);
+  echo"<script>location.href='news.php'</script>";
+?>
+  </body>
+</html>
